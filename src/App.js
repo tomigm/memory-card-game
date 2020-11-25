25f3f2e726db35bf1ab68c54726331639e30edc9
tomigm <tgmoron@gmail.com>
2020-11-25 08:32:00 -0300

    added counter, highscores and replay functionality

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import modal_2016 from './assets/images/2016_modal.png';
 import modal_2017 from './assets/images/2017_modal.png';
 import modal_2018 from './assets/images/2018_modal.png';
 import modal_2019 from './assets/images/2019_modal.png';
-import modal_2020 from './assets/images/2020_modal.png';
+
 
 
 
@@ -47,12 +47,17 @@ constructor(props) {
   this.openModal = this.openModal.bind(this)
 }
   gameFlow = () => {
-    const { counter, highScore, cardData, winScreen } = this.state;
+    const { counter, highScore } = this.state;
 
     console.log('corre');
+    if (counter > highScore){
+      this.setState({
+      highScore: counter
+      })
+    }
     if (counter === 10) {
       this.setState({
-        winScreen: !winScreen
+        winScreen: true
       })
     }
     else {
@@ -62,18 +67,17 @@ constructor(props) {
   
   handleSelection(name) {
     console.log(name);
-    const {clickedMemory, counter, highScore} = this.state;
+    const {clickedMemory, counter} = this.state;
     const values = [...clickedMemory];
-    let check = values.some(item => item === name)
-    console.log(check)
+    let check = values.some(item => item === name);
+    console.log(check);
     if(!check){
       
       this.setState({
         clickedMemory: clickedMemory.concat(name),
         counter: counter+1,
         
-      })
-      
+      }/*, () => { this.gameFlow() }*/)// ! SETSTATE SYNCHRONOUS (EXEC FUNCTION IMMEDIATELY AFTER SETSTATE) https://stackoverflow.com/questions/42018342/is-there-a-synchronous-alternative-of-setstate-in-reactjs#:~:text=There%20is%20no%20guarantee%20of,guarantee%20about%20its%20synchronous%20performance.     
     }
 
     else {
@@ -81,8 +85,6 @@ constructor(props) {
         clickedMemory: [],
         counter: 0
       })
-
-      this.gameFlow() 
     }
 
   }
@@ -93,19 +95,15 @@ constructor(props) {
         <div className="winScreen_image"></div>
                
           <div className="winScreen_btn_group">          
-            <a className="btn white">REPLAY</a>
-            <a className="btn black" target="_blank" href="https://www.pantone.com/color-intelligence/color-of-the-year/color-of-the-year-2020">MORE INFO</a>
+            <a className="btn white" href="#0" onClick = {() => this.setState({winScreen:false, clickedMemory:[], counter:0})}>REPLAY</a>
+            <a className="btn black" target="_blank"  rel="noreferrer" href="https://www.pantone.com/color-intelligence/color-of-the-year/color-of-the-year-2020">MORE INFO</a>
           </div>
-        
-
       </div>
     )
   }
 
   openModal = (event, modalImg, link) => {
-
     event.stopPropagation();
-
     this.setState({
       showModal: 'flex',
       modalContent: {img: modalImg, link: link},
@@ -127,11 +125,11 @@ constructor(props) {
         <div className="main_header">
           <img src={logo} alt="pantone main logo" className="main_header__logo"/>
           <h1>A DECADE IN COLORS</h1>
-          <a id="counter">Points: { counter }</a>
+          <a id="counter"  href="#0">Points: { counter }</a>
         </div>
         <div className="sub_header">
           <h2 className="sub_header__title">Discover & play! - Get points by clicking on each color but don't click on any more than once!</h2>
-          <a id="highScore">High Score: { highScore }</a>
+          <a id="highScore"  href="#0">High Score: { highScore }</a>
 
         </div>     
         
@@ -142,11 +140,11 @@ constructor(props) {
         this.renderOnWin()
         :
         <div>
-        <Cards cardData = {cardData}  _handleSelection={this.handleSelection.bind(this)} _openModal = {this.openModal.bind(this)} />
+        <Cards  cardData = {cardData} counter = {counter}  _handleSelection={this.handleSelection.bind(this)} _openModal = {this.openModal.bind(this)} _gameFlow = {this.gameFlow.bind(this)} />
         <div style={{display: `${showModal}`}} className="colorModal" onClick={this.closeModal} > 
           
           <div style={{backgroundImage: `url(${modalContent.img})`}} alt="modal image" className="modal_image"> </div>
-          <a className="btn white" href={modalContent.link} onClick={(e) => e.stopPropagation()} target="_blank"> MORE INFO </a> 
+          <a className="btn white" href={modalContent.link} onClick={(e) => e.stopPropagation()} rel="noreferrer" target="_blank"> MORE INFO </a> 
           
         </div>
        </div>
--- src/components/cards.js
+++ src/components/cards.js
@@ -6,35 +6,36 @@ class Cards extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            
+        this.state = {            
             isOpen: false,
-
         }
     }
 
-
     cardSort = (array) => {
-        
         array.sort(() => Math.random() - 0.5);
-        
-        
         return array
-
     }
-
+// Every time the counter adds +1, the cards are sorted
+    componentDidUpdate(prevProps) {
+        if (this.props.counter !== prevProps.counter ){
+          this.props._gameFlow();
+          const cardsSorted = this.cardSort(this.props.cardData);
+          this.setState({
+            cardData: cardsSorted
+          })
+        }
+      }
+// First time that cards are mounted; they are sorted after mounted
     componentDidMount() {
         const  cardData  = this.props.cardData;
         const cardsSorted = this.cardSort(cardData)
         this.setState({
             cardData: cardsSorted
-
-        })
+        })        
     }
 
     cardGen = () => {
-        const cardData = this.props.cardData
-        const { isOpen} = this.state
+        const cardData = this.props.cardData        
         return cardData.map(card =>  (
             <div className="card" name={card.name} onClick={(event) => this.props._handleSelection(card.name, event)}>
                 <div className="card__colorBox"  style={{backgroundColor: `${card.color}`}} >
@@ -44,21 +45,18 @@ class Cards extends React.Component {
                     <img src={logo} alt="pantone card logo" className="description__logo" />
                     <p>{card.code}</p>
                     <p>{card.name}</p>
-                </div>
-                
+                </div>                
             </div>
         ))
     }
 
-    render() {
-        const {cardData, isOpen} = this.state
+    render() {        
         return (
             <div className="card_container">
             {this.cardGen()}
             </div>
         )
     }
-
 }
 
 export default Cards
